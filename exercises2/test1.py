041e15a01ac4a25a8cd54955a83435754e3122f7
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-06-15 18:17:11 +0200

    data structures exercises

---
+++ exercises2/test1.py
@@ -0,0 +1,72 @@
+# DATA STRUCTURES exercises
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+even_times_two = [x * 2 for x in numbers if x % 2 == 0]
+# print(even_times_two)
+
+# ---
+
+user = {
+    "name": "Adrian",
+    "age": 25,
+    "city": "Warsaw"
+}
+
+# for key, value in user.items():
+#   print(f"{key} -> {value}")
+
+# ---
+
+words = ["apple", "banana", "apple", "orange", "banana", "apple"]
+
+fruits = {}
+
+for item in words:
+  if item in fruits.keys():
+    fruits[item] += 1
+  else:
+    fruits.update({item: 1})
+
+# print(fruits)
+
+# ---
+
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+
+sort = sorted(set(numbers))
+print(sort)
+
+# ---
+
+numbers = [10, 15, 20, 25, 30, 35, 40]
+
+even_odd_dict = {
+  "even": [],
+  "odd": []
+}
+
+for item in numbers:
+  if item % 2 == 0:
+    even_odd_dict["even"].append(item)
+  else:
+    even_odd_dict["odd"].append(item)
+
+print(even_odd_dict)
+
+# ---
+
+text = "programming in python is fun but programming is also challenging"
+
+check = {}
+
+for item in text.split():
+  if item not in check:
+    check[item] = 1
+  else:
+    check[item] += 1
+
+for item in text.split():
+  if check[item] == 1:
+    print(item)
+    break
